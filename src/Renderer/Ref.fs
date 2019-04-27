ee1b5fd07a59510b865761a5e2afb136a4dbebd8
sc <siu.chan16@imperial.ac.uk>
2019-04-27 12:59:43 +0100

    Clean up

--- src/Renderer/Ref.fs
+++
@@ -1,9 +0,0 @@
-(* 
-    High Level Programming @ Imperial College London # Spring 2018
-    Project: A user-friendly ARM emulator in F# and Web Technologies ( Github Electron & Fable Compliler )
-    Contributors: Angelos Filos
-    Module: Ref
-    Description: References to `HTML` elements from `index.html`.
-*)
-
-module Ref
