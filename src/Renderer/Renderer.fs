eb7587aa5ca1d523bedb6b32bd0008661423f145
sc <siu.chan16@imperial.ac.uk>
2019-04-30 17:25:50 +0100

    Clean up Renderer.fs

--- src/Renderer/Renderer.fs
+++ src/Renderer/Renderer.fs
@@ -7,55 +7,19 @@ open Elmish.Debug
 open Elmish.Browser.Navigation
 open Elmish.Browser.UrlParser
 
-type CModel =
+type Model =
     { Count : int
       IsAlertVisible : bool }
 
-type PageModel =
-    | CounterPage of CModel
-    | AlertsPage
-
-type Message =
-    | Increase
-    | Decrease
-    | HideAlert
-    | ShowAlert
-
-type ActiveTab =
-    | One
-    | Two
-    | Three
-
-type Model =
-    { PageModel : PageModel
-      ActiveTab : ActiveTab }
-
-let init2 () =
-    { Count = 42
-      IsAlertVisible = true }
-
-type Route =
-    | Counter
-    | Alerts
-
 type Msg =
     | CounterMsg
-    | ChangeAsideTab of ActiveTab
+    | ChangeAsideTab
 
-let route : Parser<Route -> Route, Route> =
-    oneOf
-        [ map Counter (s "counter")
-          map Alerts (s "alerts") ]
-
-let urlUpdate (result : Route option) model =
-    model , Cmd.none
-
-let init result =
-    let cm = init2 ()
-    let model = { PageModel = CounterPage cm
-                  ActiveTab = One }
-
-    urlUpdate result model
+let init _ =
+        { 
+            Count = 42
+            IsAlertVisible = true
+        }, Cmd.none
 
 let update (msg : Msg) (model : Model) =
     model, Cmd.none
@@ -64,16 +28,11 @@ module R = Fable.Helpers.React
 
 let view (model : Model) (dispatch : Msg -> unit) =
     R.div [] [ 
-        R.str "Elmish testing"]
+        R.str "Elmish testing testing2"]
 
 Program.mkProgram init update view
-|> Program.toNavigable (parseHash route) urlUpdate
 #if DEBUG
 |> Program.withHMR
-//|> Program.withDebugger
 #endif
 |> Program.withReact "app"
-#if DEBUG
-|> Program.withConsoleTrace
-#endif
 |> Program.run
